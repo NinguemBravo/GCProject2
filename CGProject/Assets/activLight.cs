4e957c98315aa38d4fbf181eaa3115ed64252168
Andre-Heiden-Gugelmin <andre_gugelmin@hotmail.com>
2020-06-12 16:44:10 -0300

    Projeto

---
+++ CGProject/Assets/activLight.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activLight : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject light;
+    [SerializeField]
+    private GameObject postFx;
+    [SerializeField]
+    private GameObject particles;
+    private bool lightOn;
+    private bool particleOn;
+    private bool postFXOn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            lightOn = light.activeInHierarchy;
+            light.SetActive(!lightOn);
+        }
+        if (Input.GetKeyDown("left shift"))
+        {
+            postFXOn = postFx.activeInHierarchy;
+            postFx.SetActive(!postFXOn);
+        }
+        if (Input.GetKeyDown("left ctrl"))
+        {
+            particleOn = particles.activeInHierarchy;
+            particles.SetActive(!particleOn);
+        }
+    }
+}
